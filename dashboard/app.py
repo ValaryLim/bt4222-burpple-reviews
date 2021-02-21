97b04236536766602a6bdccce672fb3f371ec8bd
Lai Yan Jean <44203524+laiyanjean@users.noreply.github.com>
2021-02-21 09:49:11 +0800

    Create dashboard filtering restaurants by location and price (#7)
    * Create dashboard filtering restaurants by location and price
    
    * Organise dashboard project structure

---
+++ dashboard/app.py
@@ -0,0 +1,7 @@
+# import packages
+import dash
+import dash_bootstrap_components as dbc
+
+# set up application
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+# server = app.server
