3338bde59f6c14a63b9adb79bfdb83cd86d6ebd1
laiyanjean <laiyanjean@gmail.com>
2021-02-22 23:29:19 +0800

    Add aspect filter functionality to dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,5 +3,5 @@ import dash
 import dash_bootstrap_components as dbc
 
 # set up application
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 # server = app.server
