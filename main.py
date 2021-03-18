7feecd7aab687baf3275fa69cbd875737e678e99
risalim <42958749+risalim@users.noreply.github.com>
2021-03-18 16:53:23 +0800

    Update main.py import utils

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 ################
 ### packages ###
 ################
-import sys
-
-#################
-### functions ###
-#################
-sys.path.insert(1, './scraping/utils')
-from scrapingUtils import load_url, format_search_terms, format_review_date, compile_csv, compile
-sys.path.insert(1, './rules/utils')
-from preprocessing import clean_review
-from rules import get_all_aspects, get_pos, get_aspects, ranges, pos_before_after_aspect, get_sentence_indexes, get_sentences_indexes, get_sentences, process_review_aspect, add_phrases, process_reviews
-from postprocessing import process_csv_lists, process_categories, one_hot_encode_emojis, clean_phrase
+import utils
 
 ################
 ### scraping ###
