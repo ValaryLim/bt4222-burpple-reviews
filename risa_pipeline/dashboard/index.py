8c5f21c037a2d873511ca4e3eb502c83aff93f08
risalim <42958749+risalim@users.noreply.github.com>
2021-03-21 18:42:26 +0800

    Create index.py

---
+++ risa_pipeline/dashboard/index.py
@@ -0,0 +1,33 @@
+# import packages
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+# import apps
+from app import app
+from apps import main_page, restaurant_page
+
+#### APP LAYOUT ##########################################################
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+
+#### APP CALLBACKS ##########################################################
+@app.callback(Output('page-content', 'children'),
+              Input('url', 'pathname'))
+
+def display_page(pathname):
+    if pathname == '/':
+        return main_page.layout
+    elif '/restaurant-' in pathname:
+        return restaurant_page.layout
+    #     return # something
+    # else: # default start page
+    #     return search_restaurants.layout
+    else:
+        return '404'
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
