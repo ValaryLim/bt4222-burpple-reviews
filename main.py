a984a97a31cf3beca6aa1390854f5ce84e5ad23b
Lai Yan Jean <44203524+laiyanjean@users.noreply.github.com>
2021-04-02 09:41:53 +0800

    Ran predictions part of pipeline (#52)
    * Add checkpoints to pipeline
    
    * Ran predictions on models using pipeline
    
    * Update BERT Model Path

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ RESTAURANT_DETAILED_CSV = "data/pipeline/restaurants_detailed.csv"
 REVIEW_CSV = "data/pipeline/reviews.csv"
 PREPROCESSED_CSV = "data/pipeline/reviews_preprocessed.csv"
 RULE_MINED_CSV = "data/pipeline/rule_mined.csv"
-POSTPROCESSED_CSV = "data/pipeline/reviews_postprocessed.csv"
+# POSTPROCESSED_CSV = "data/pipeline/reviews_postprocessed.csv"
+# checkpoint 4 contains predictions for all models except VADER
+POSTPROCESSED_CSV = "data/pipeline/baseline_prediction_checkpoint4.csv"
 BASELINE_CSV = "data/pipeline/baseline_prediction.csv"
 ENSEMBLE_CSV = "data/pipeline/ensemble_prediction.csv"
 REVIEW_FINAL = "data/pipeline/reviews_final.csv"
@@ -19,22 +21,22 @@ RESTAURANT_FINAL = "data/pipeline/restaurants_final.csv"
 
 if __name__ == "__main__":
     # scrape
-    scraping.scraping_pipeline(RESTAURANT_CSV, RESTAURANT_DETAILED_CSV, REVIEW_CSV)
+    # scraping.scraping_pipeline(RESTAURANT_CSV, RESTAURANT_DETAILED_CSV, REVIEW_CSV)
     
     # preprocessing
-    utils.preprocessing_pipeline(REVIEW_CSV, PREPROCESSED_CSV)
+    # utils.preprocessing_pipeline(REVIEW_CSV, PREPROCESSED_CSV)
 
     # rule mining
-    utils.rule_mining_pipeline(PREPROCESSED_CSV, RULE_MINED_CSV) 
+    # utils.rule_mining_pipeline(PREPROCESSED_CSV, RULE_MINED_CSV) 
 
     # postprocessing
-    utils.postprocessing_pipeline(RULE_MINED_CSV, POSTPROCESSED_CSV) 
+    # utils.postprocessing_pipeline(RULE_MINED_CSV, POSTPROCESSED_CSV) 
 
     # modelling
     modelling.base_modelling_pipeline(POSTPROCESSED_CSV, BASELINE_CSV)
     # modelling.meta_modelling_pipeline(PREDICTIONS_CSV, ENSEMBLE_CSV)
 
     # scoring
-    utils.scoring_pipeline(ENSEMBLE_CSV, RESTAURANT_DETAILED_CSV, REVIEW_FINAL, RESTAURANT_FINAL)
+    # utils.scoring_pipeline(ENSEMBLE_CSV, RESTAURANT_DETAILED_CSV, REVIEW_FINAL, RESTAURANT_FINAL)
 
     # ##### dashboard to read data from REVIEW_FINAL, RESTAURANT_FINAL
